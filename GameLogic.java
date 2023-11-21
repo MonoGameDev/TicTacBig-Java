0e140c17387e1824a66483d69504dd97be6a05de
vegal01 <x@gmail.com>
2023-11-21 10:43:35 +0100

    setSymbol function

--- GameLogic.java
+++ GameLogic.java
@@ -1,11 +1,17 @@
+import java.util.ArrayList;
+
 public class GameLogic {
-    Symbol[] symbolArray;
+
+    ArrayList<Symbol>symbolList = new ArrayList<Symbol>();
     public Symbol getSymbol(int x, int y){
-        for (Symbol symbol : symbolArray) {
+        for (Symbol symbol : symbolList) {
             if (symbol.x == x && symbol.y == y) {
                 return symbol;
             }
         }
         return null;
+    }
+    public void setSymbol(Symbol player){
+        symbolList.add(player);
     }
 }
